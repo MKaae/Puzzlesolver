c60bd2c310648a41c91d3b65057c523574ff1d05
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-01-07 16:03:36 +0100

    Final PuzzleSolved

--- src/SolutionsForUniqueChars.java
+++ src/SolutionsForUniqueChars.java
@@ -3,33 +3,26 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class SolutionsForUniqueChars {
-    public void UniqueLetters(){
+    public void UniqueLetters() {
         ArrayList<String> tempArr = new ArrayList<>(1017);
-        try{
+        try {
             File fl = new File("NewFile3Words.txt");
             Scanner sc = new Scanner(fl);
-            while(sc.hasNextLine()) {
+            while (sc.hasNextLine()) {
                 String temp = sc.nextLine();
                 tempArr.add(temp);
             }
-        }catch(FileNotFoundException FNFE){
+        } catch (FileNotFoundException FNFE) {
             System.out.println("File not found.");
         }
         int tempArrSize = tempArr.size();
-        for(int i = 1; i < tempArrSize; i++){
-            for (int j = 0; j < tempArrSize; j++) {
-                if (tempArr.get(i).charAt(0) == tempArr.get(j).charAt(0)){
-                    tempArr.remove(i);
-                    tempArrSize = tempArrSize-1;
-                }
-                else if(tempArr.get(i).charAt(1) == tempArr.get(j).charAt(1)){
-                    tempArr.remove(i);
-                    tempArrSize = tempArrSize-1;
-                }
-                else if(tempArr.get(i).charAt(2) == tempArr.get(j).charAt(2)){
-                    tempArr.remove(i);
-                    tempArrSize = tempArrSize-1;
-                }
+        for (int i = 0; i < tempArrSize-1; i++) {
+            if (tempArr.get(i+1).charAt(0) == tempArr.get(i).charAt(0) ||
+                    tempArr.get(i+1).charAt(1) == tempArr.get(i).charAt(1) ||
+                    tempArr.get(i+1).charAt(2) == tempArr.get(i).charAt(2)) {
+                tempArr.remove(i+1);
+                tempArrSize = tempArrSize - 1;
+                i--;
             }
         }
         System.out.println(tempArr.size());
